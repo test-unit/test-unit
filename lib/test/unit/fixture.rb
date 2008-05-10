839bf8f79c03bf48f9a5bdf14a84f9dd962b9219
Kouhei Sutou <kou@cozmixng.org>
2008-05-10 12:58:06 +0000

    * abstract.

--- lib/test/unit/fixture.rb
+++ lib/test/unit/fixture.rb
@@ -4,104 +4,82 @@ module Test
       class << self
         def included(base)
           base.extend(ClassMethods)
-          observe_setup = Proc.new do |test_case, _, _, value, method_name|
-            if value
-              test_case.register_setup_method(method_name)
-            else
-              test_case.unregister_setup_method(method_name)
+
+          [:setup, :teardown].each do |fixture|
+            observer = Proc.new do |test_case, _, _, value, method_name|
+              if value
+                test_case.send("register_#{fixture}_method", method_name)
+              else
+                test_case.send("unregister_#{fixture}_method", method_name)
+              end
             end
+            base.register_attribute_observer(fixture, &observer)
           end
-          observe_teardown = Proc.new do |test_case, _, _, value, method_name|
-            if value
-              test_case.register_teardown_method(method_name)
-            else
-              test_case.unregister_teardown_method(method_name)
-            end
-          end
-          base.register_attribute_observer(:setup, &observe_setup)
-          base.register_attribute_observer(:teardown, &observe_teardown)
         end
       end
 
       module ClassMethods
-        def setup(*method_names)
-          attribute(:setup, true, *method_names)
-        end
+        [:setup, :teardown].each do |fixture|
+          class_eval do
+            define_method(fixture) do |*method_names|
+              attribute(fixture, true, *method_names)
+            end
 
-        def unregister_setup(*method_names)
-          attribute(:setup, false, *method_names)
-        end
-
-        def teardown(*method_names)
-          attribute(:teardown, true, *method_names)
-        end
-
-        def unregister_teardown(*method_names)
-          attribute(:teardown, false, *method_names)
-        end
-
-        def register_setup_method(method_name)
-          @setup_methods ||= []
-          @setup_methods |= [method_name]
-        end
-
-        def unregister_setup_method(method_name)
-          @unregistered_setup_methods ||= []
-          @unregistered_setup_methods |= [method_name]
-        end
-
-        def setup_methods
-          interested_ancestors = ancestors[0, ancestors.index(Fixture)].reverse
-          interested_ancestors.inject([]) do |result, ancestor|
-            if ancestor.is_a?(Class)
-              ancestor.class_eval do
-                @setup_methods ||= []
-                @unregistered_setup_methods ||= []
-                result + @setup_methods - @unregistered_setup_methods
-              end
-            else
-              []
+            define_method("unregister_#{fixture}") do |*method_names|
+              attribute(fixture, false, *method_names)
             end
           end
-        end
 
-        def register_teardown_method(method_name)
-          @teardown_methods ||= []
-          @teardown_methods |= [method_name]
-        end
-
-        def unregister_teardown_method(method_name)
-          @unregistered_teardown_methods ||= []
-          @unregistered_teardown_methods |= [method_name]
-        end
-
-        def teardown_methods
-          interested_ancestors = ancestors[0, ancestors.index(Fixture)].reverse
-          interested_ancestors.inject([]) do |result, ancestor|
-            if ancestor.is_a?(Class)
-              ancestor.class_eval do
-                @teardown_methods ||= []
-                @unregistered_teardown_methods ||= []
-                result + @teardown_methods - @unregistered_teardown_methods
+          append_method_name = Proc.new do |variable_name|
+            Proc.new do |method_name|
+              unless instance_variable_defined?(variable_name)
+                instance_variable_set(variable_name, [])
               end
-            else
-              []
+              methods = instance_variable_get(variable_name) | [method_name]
+              instance_variable_set(variable_name, methods)
+            end
+          end
+
+          methods_name = "@#{fixture}_methods"
+          define_method("register_#{fixture}_method",
+                        &append_method_name.call(methods_name))
+
+          unregistered_methods_name = "@unregistered_#{fixture}_methods"
+          define_method("unregister_#{fixture}_method",
+                        &append_method_name.call(unregistered_methods_name))
+
+          define_method("#{fixture}_methods") do
+            interested_ancestors = ancestors[0, ancestors.index(Fixture)].reverse
+            interested_ancestors.inject([]) do |result, ancestor|
+              if ancestor.is_a?(Class)
+                ancestor.class_eval do
+                  methods = []
+                  unregistered_methods = []
+                  if instance_variable_defined?(methods_name)
+                    methods = instance_variable_get(methods_name)
+                  end
+                  if instance_variable_defined?(unregistered_methods_name)
+                    unregistered_methods =
+                      instance_variable_get(unregistered_methods_name)
+                  end
+                  result + methods - unregistered_methods
+                end
+              else
+                []
+              end
             end
           end
         end
       end
 
-      private
-      def run_setup
-        [:setup, *self.class.setup_methods].each do |method_name|
-          send(method_name) if respond_to?(method_name, true)
+      [:setup, :teardown].each do |fixture|
+        fixture_runner = "run_#{fixture}"
+        define_method(fixture_runner) do
+          [fixture, *self.class.send("#{fixture}_methods")].each do |method_name|
+            send(method_name) if respond_to?(method_name, true)
+          end
         end
-      end
-
-      def run_teardown
-        [:teardown, *self.class.teardown_methods].each do |method_name|
-          send(method_name) if respond_to?(method_name, true)
-        end
+        send(:private, fixture_runner)
       end
     end
   end
