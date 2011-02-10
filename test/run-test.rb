45475d43c0a90d3b7758958251796fa76e2551f7
Kouhei Sutou <kou@cozmixng.org>
2011-02-10 02:19:27 +0000

    * use test-unit-notify if it's available.

--- test/run-test.rb
+++ test/run-test.rb
@@ -12,4 +12,11 @@ $LOAD_PATH.unshift(lib_dir)
 
 require 'test/unit'
 
+test_unit_notify_base_dir = File.join(base_dir, "..", "test-unit-notify")
+test_unit_notify_base_dir = File.expand_path(test_unit_notify_base_dir)
+if File.exist?(test_unit_notify_base_dir)
+  $LOAD_PATH.unshift(File.join(test_unit_notify_base_dir, "lib"))
+  require 'test/unit/notify'
+end
+
 exit Test::Unit::AutoRunner.run(true, test_dir)
