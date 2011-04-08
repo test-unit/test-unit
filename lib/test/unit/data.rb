6263ee25644057d02a785cfac8cd70f52d55ab7d
Kouhei Sutou <kou@cozmixng.org>
2011-04-08 14:38:57 +0000

    * split CSV data loader.

--- lib/test/unit/data.rb
+++ lib/test/unit/data.rb
@@ -25,19 +25,51 @@ module Test
           attribute(:data, current_data + [data_set])
         end
 
-        def load_data(filename)
-          case filename
+        def load_data(file_name)
+          case file_name
           when /\.csv/i
-            require 'csv'
-            CSV.foreach(filename,
-                        :headers => true,
-                        :converters => :numeric) do |row|
-              data = row.to_hash
-              label = data.delete("label")
-              data(label, data)
-            end
+            loader = CSVDataLoader.new(self)
+            loader.load(file_name)
           else
             raise ArgumentError, "unsupported file format: <#{filename}>"
+          end
+        end
+
+        class CSVDataLoader
+          def initialize(test_case)
+            @test_case = test_case
+          end
+
+          def load(file_name)
+            require 'csv'
+            header = nil
+            CSV.foreach(file_name) do |row|
+              if header.nil?
+                header = row
+                next
+              end
+              label = nil
+              data = {}
+              header.each_with_index do |key, i|
+                if key == "label"
+                  label = row[i]
+                else
+                  data[key] = normalize_value(row[i])
+                end
+              end
+              @test_case.data(label, data)
+            end
+          end
+
+          private
+          def normalize_value(value)
+            Integer(value)
+          rescue ArgumentError
+            begin
+              Float(value)
+            rescue ArgumentError
+              value
+            end
           end
         end
       end
