6a5dfe83b36065bc98e0db5ebe5a205afcda21db
Kouhei Sutou <kou@clear-code.com>
2012-03-03 19:11:54 +0900

    Use meaningful name

--- test/test-code-snippet.rb
+++ test/test-code-snippet.rb
@@ -1,30 +1,32 @@
-require 'test/unit'
-require 'stringio'
+require "test-unit"
+require "stringio"
 
-class TestJRuby < Test::Unit::TestCase
-  class TestCase < Test::Unit::TestCase
+class TestCodeSnippet < Test::Unit::TestCase
+  class TestErrorInsideJRuby < Test::Unit::TestCase
     class << self
       def suite
         Test::Unit::TestSuite.new(name)
       end
     end
 
-    def test_java_exception_output
+    def test_exception_from_java
       require 'java'
-      java.util.Vector.new -1
+      java.util.Vector.new(-1)
     end
   end
 
-  def test_java_exception_output
+  def test_error_inside_jruby
     omit("Skipping test for JRuby") unless RUBY_PLATFORM =~ /java/
 
-    suite = TestCase.suite
-    suite << TestCase.new('test_java_exception_output')
+    suite = TestErrorInsideJRuby.suite
+    suite << TestErrorInsideJRuby.new("test_exception_from_java")
 
     output = StringIO.new
     runner = Test::Unit::UI::Console::TestRunner.new(suite, :output => output)
     result = runner.start
 
-    assert_equal 1, result.faults.size
+    assert_equal("1 tests, 0 assertions, 0 failures, " +
+                 "1 errors, 0 pendings, 0 omissions, 0 notifications",
+                 result.summary)
   end
 end
