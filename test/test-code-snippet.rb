5e0ecfcbdcac116673453bc89937e3f31246fe55
Kouhei Sutou <kou@clear-code.com>
2012-03-03 19:27:05 +0900

    Don't use test runner for CodeSnippetFechter test

--- test/test-code-snippet.rb
+++ test/test-code-snippet.rb
@@ -1,32 +1,37 @@
 require "test-unit"
-require "stringio"
+require "testunit-test-util"
 
 class TestCodeSnippet < Test::Unit::TestCase
-  class TestErrorInsideJRuby < Test::Unit::TestCase
-    class << self
-      def suite
-        Test::Unit::TestSuite.new(name)
+  include TestUnitTestUtil
+
+  class TestJRuby < self
+    def test_error_inside_jruby
+      jruby_only_test
+
+      backtrace = backtrace_from_jruby
+      no_rb_entries = backtrace.find_all do |(file,)|
+        File.extname(file) != ".rb"
+      end
+
+      fetcher = Test::Unit::CodeSnippetFetcher.new
+      snippets = no_rb_entries.collect do |(file, line, _)|
+        fetcher.fetch(file, line)
+      end
+      assert_equal([[]] * no_rb_entries.size,
+                   snippets)
+    end
+
+    private
+    def backtrace_from_jruby
+      begin
+        java.util.Vector.new(-1)
+      rescue Exception
+        $@.collect do |entry|
+          entry.split(/:/, 3)
+        end
+      else
+        flunk("failed to raise an exception from JRuby.")
       end
     end
-
-    def test_exception_from_java
-      require 'java'
-      java.util.Vector.new(-1)
-    end
-  end
-
-  def test_error_inside_jruby
-    omit("Skipping test for JRuby") unless RUBY_PLATFORM =~ /java/
-
-    suite = TestErrorInsideJRuby.suite
-    suite << TestErrorInsideJRuby.new("test_exception_from_java")
-
-    output = StringIO.new
-    runner = Test::Unit::UI::Console::TestRunner.new(suite, :output => output)
-    result = runner.start
-
-    assert_equal("1 tests, 0 assertions, 0 failures, " +
-                 "1 errors, 0 pendings, 0 omissions, 0 notifications",
-                 result.summary)
   end
 end
--- test/testunit-test-util.rb
+++ test/testunit-test-util.rb
@@ -1,5 +1,13 @@
 module TestUnitTestUtil
   private
+  def jruby_only_test
+    begin
+      require "java"
+    rescue LoadError
+      omit("test for JRuby")
+    end
+  end
+
   def assert_fault_messages(expected, faults)
     assert_equal(expected, faults.collect {|fault| fault.message})
   end
