dd7fd2bafa17186d4d73b3802c33eecb5dff3ace
Kouhei Sutou <kou@clear-code.com>
2012-03-31 18:17:19 +0900

    Add experimental top-level 'run' method
    It is for "ruby -rtest-unit -e run test/test_*.rb".

--- lib/test-unit.rb
+++ lib/test-unit.rb
@@ -4,3 +4,10 @@ module Test
     autoload :AutoRunner, "test/unit/autorunner"
   end
 end
+
+# experimental. It is for "ruby -rtest-unit -e run test/test_*.rb".
+# Is this API OK or dirty?
+def run
+  self.class.send(:undef_method, :run)
+  require "test/unit"
+end
