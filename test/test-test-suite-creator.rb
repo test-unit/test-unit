44da7d0961f07bdd2407d9e9f65f191649018a5a
Kouhei Sutou <kou@clear-code.com>
2014-01-25 18:01:55 +0900

    Extract test for including and inheritance case

--- test/test-test-suite-creator.rb
+++ test/test-test-suite-creator.rb
@@ -60,29 +60,30 @@ module Test
         end
       end
 
-      def setup
-        @test_module = Module.new do
-          def test_0; end
+      class TestInheritedModule < self
+        def setup
+          test_module = Module.new do
+            def test_in_module
+            end
+          end
+
+          @parent_test_case = Class.new(TestCase) do
+            include test_module
+
+            def test_in_parent
+            end
+          end
+
+          @child_test_case = Class.new(@parent_test_case) do
+            def test_in_child
+            end
+          end
         end
 
-        @test_case1 = Class.new(TestCase) do
-          self.test_order = :alphabetic
-          def test_1; end
+        def test_collect_test_names
+          assert_equal(["test_in_child"],
+                       collect_test_names(@child_test_case))
         end
-
-        @test_case2 = Class.new(@test_case1) do
-          def test_2; end
-        end
-      end
-
-      def test_skips_collecting_inherited_tests
-        creator = TestSuiteCreator.new @test_case2
-        assert_equal(["test_2"], creator.send(:collect_test_names))
-
-        @test_case1.send(:include, @test_module)
-
-        creator = TestSuiteCreator.new @test_case2
-        assert_equal(["test_2"], creator.send(:collect_test_names))
       end
     end
   end
