6026ce8f64d341441c2e06eb79e4852c84ca23be
Kouhei Sutou <kou@clear-code.com>
2014-01-25 17:55:04 +0900

    Add a test for inherited test case case

--- test/test-test-suite-creator.rb
+++ test/test-test-suite-creator.rb
@@ -3,6 +3,30 @@ require "test/unit"
 module Test
   module Unit
     class TestTestSuiteCreator < TestCase
+      def collect_test_names(test_case)
+        creator = TestSuiteCreator.new(test_case)
+        creator.send(:collect_test_names)
+      end
+
+      class TestInherited < self
+        def setup
+          @parent_test_case = Class.new(TestCase) do
+            self.test_order = :alphabetic
+            def test_in_parent
+            end
+          end
+
+          @child_test_case = Class.new(@parent_test_case) do
+            def test_in_child
+            end
+          end
+        end
+
+        def test_collect_test_names
+          assert_equal(["test_in_child"], collect_test_names(@child_test_case))
+        end
+      end
+
       def setup
         @test_module = Module.new do
           def test_0; end
