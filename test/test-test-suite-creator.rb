bc6ebba12c958aa6d6a2fef0ce0dcaa4cc8644bd
Kouhei Sutou <kou@clear-code.com>
2014-01-25 17:50:18 +0900

    Remove needless internal spaces in array literal

--- test/test-test-suite-creator.rb
+++ test/test-test-suite-creator.rb
@@ -20,24 +20,24 @@ module Test
 
       def test_collects_tests
         creator = TestSuiteCreator.new @test_case1
-        assert_equal([ "test_1" ], creator.send(:collect_test_names))
+        assert_equal(["test_1"], creator.send(:collect_test_names))
       end
 
       def test_collects_included_tests
         @test_case1.send(:include, @test_module)
 
         creator = TestSuiteCreator.new @test_case1
-        assert_equal([ "test_0", "test_1" ], creator.send(:collect_test_names))
+        assert_equal(["test_0", "test_1"], creator.send(:collect_test_names))
       end
 
       def test_skips_collecting_inherited_tests
         creator = TestSuiteCreator.new @test_case2
-        assert_equal([ "test_2" ], creator.send(:collect_test_names))
+        assert_equal(["test_2"], creator.send(:collect_test_names))
 
         @test_case1.send(:include, @test_module)
 
         creator = TestSuiteCreator.new @test_case2
-        assert_equal([ "test_2" ], creator.send(:collect_test_names))
+        assert_equal(["test_2"], creator.send(:collect_test_names))
       end
     end
   end
