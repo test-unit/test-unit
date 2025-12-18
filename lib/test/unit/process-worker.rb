252807050e1e0943cbc7de2a978a5ab7b809a649
Tsutomu Katsube <tsuto.katsube@gmail.com>
2025-12-18 09:34:10 +0900

    Add support for process based parallel test running on Windows (#348)
    GitHub: GH-235, GH-346
    
    On Windows, file descriptors 3 and above cannot be passed to child
    processes. So we use `TCPSocket` instead of `IO.pipe`.
    
    ---------
    
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

--- lib/test/unit/process-worker.rb
+++ lib/test/unit/process-worker.rb
@@ -5,6 +5,7 @@
 # License:: Ruby license.
 
 require "optparse"
+require "socket"
 
 parser = OptionParser.new
 parser.on("--load-path=PATH") do |path|
@@ -18,6 +19,14 @@ worker_id = nil
 parser.on("--worker-id=ID", Integer) do |id|
   worker_id = id
 end
+remote_ip_address = nil
+parser.on("--ip-address=ADDRESS") do |address|
+  remote_ip_address = address
+end
+remote_ip_port = nil
+parser.on("--ip-port=PORT", Integer) do |port|
+  remote_ip_port = port
+end
 test_paths = parser.parse!
 
 require_relative "../unit"
@@ -29,34 +38,46 @@ collector = Test::Unit::Collector::Load.new
 collector.base = base_directory
 suite = collector.collect(*test_paths)
 
-IO.open(Test::Unit::TestSuiteProcessRunner::MAIN_TO_WORKER_INPUT_FILENO) do |data_input|
-  IO.open(Test::Unit::TestSuiteProcessRunner::WORKER_TO_MAIN_OUTPUT_FILENO) do |data_output|
-    loop do
-      Marshal.dump({status: :ready}, data_output)
-      data_output.flush
-      test_name = Marshal.load(data_input)
-      break if test_name.nil?
-      test = suite.find(test_name)
-      result = Test::Unit::ProcessTestResult.new(data_output)
-      run_context = Test::Unit::TestRunContext.new(Test::Unit::TestSuiteRunner)
-
-      event_listener = lambda do |event_name, *args|
-        Marshal.dump({status: :event, event_name: event_name, args: args}, data_output)
-        data_output.flush
-      end
-      if test.is_a?(Test::Unit::TestSuite)
-        test_suite = test
-      else
-        test_suite = Test::Unit::TestSuite.new(test.class.name, test.class)
-        test_suite << test
-      end
-      runner = Test::Unit::TestSuiteRunner.new(test_suite)
-      worker_context = Test::Unit::WorkerContext.new(worker_id, run_context, result)
-      runner.run(worker_context, &event_listener)
+io_open = lambda do |&block|
+  if Gem.win_platform?
+    TCPSocket.open(remote_ip_address, remote_ip_port) do |data_socket|
+      block.call(data_socket, data_socket)
     end
-    Marshal.dump({status: :done}, data_output)
-    data_output.flush
+  else
+    IO.open(Test::Unit::TestSuiteProcessRunner::MAIN_TO_WORKER_INPUT_FILENO) do |data_input|
+      IO.open(Test::Unit::TestSuiteProcessRunner::WORKER_TO_MAIN_OUTPUT_FILENO) do |data_output|
+        block.call(data_input, data_output)
+      end
+    end
   end
+end
+
+io_open.call do |data_input, data_output|
+  loop do
+    Marshal.dump({status: :ready}, data_output)
+    data_output.flush
+    test_name = Marshal.load(data_input)
+    break if test_name.nil?
+    test = suite.find(test_name)
+    result = Test::Unit::ProcessTestResult.new(data_output)
+    run_context = Test::Unit::TestRunContext.new(Test::Unit::TestSuiteRunner)
+
+    event_listener = lambda do |event_name, *args|
+      Marshal.dump({status: :event, event_name: event_name, args: args}, data_output)
+      data_output.flush
+    end
+    if test.is_a?(Test::Unit::TestSuite)
+      test_suite = test
+    else
+      test_suite = Test::Unit::TestSuite.new(test.class.name, test.class)
+      test_suite << test
+    end
+    runner = Test::Unit::TestSuiteRunner.new(test_suite)
+    worker_context = Test::Unit::WorkerContext.new(worker_id, run_context, result)
+    runner.run(worker_context, &event_listener)
+  end
+  Marshal.dump({status: :done}, data_output)
+  data_output.flush
 
   Marshal.load(data_input)
 end
