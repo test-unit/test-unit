82e87eb9c1d0cb1c61dfc129c3fbd32fcfaacc5a
Tsutomu Katsube <tsuto.katsube@gmail.com>
2026-07-20 21:04:17 +0900

    Add :message in require_failed_infos (#386)
    To avoid unnecessary exception objects.
    
    ---------
    
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

--- lib/test/unit/collector/load.rb
+++ lib/test/unit/collector/load.rb
@@ -181,10 +181,17 @@ module Test
               normalized_path = path.to_s.gsub(/[^a-z0-9\_]+/i, '_')
               normalized_path = normalized_path.gsub(/\A_+/, '')
               exception = info[:exception]
-              define_method("test_require_#{normalized_path}") do
-                raise(exception.class,
-                      "failed to load <#{path}>: #{exception.message}",
-                      exception.backtrace)
+              if exception
+                define_method("test_require_#{normalized_path}") do
+                  raise(exception.class,
+                        "failed to load <#{path}>: #{exception.message}",
+                        exception.backtrace)
+                end
+              else
+                message = info[:message]
+                define_method("test_require_#{normalized_path}") do
+                  raise("failed to load <#{path}>: #{message}")
+                end
               end
             end
 
