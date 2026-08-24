1d604a14835a3e12a2f3ec522298931da56c9926
Sutou Kouhei <kou@cozmixng.org>
2026-08-24 10:06:46 +0900

    Fix order

--- lib/test/unit/worker-context.rb
+++ lib/test/unit/worker-context.rb
@@ -21,6 +21,20 @@ module Test
         def test_result?
           false
         end
+      class << self
+        # Returns false to not re-wrap this by WorkerContext
+        def test_result?
+          false
+        end
+      end
+
+      attr_reader :id
+      attr_reader :run_context
+      attr_reader :result
+      def initialize(id, run_context, result)
+        @id = id
+        @run_context = run_context
+        @result = result
       end
     end
   end
